cbb1c3e91cfe270c023412003eee2a9c8af2b5b0
jxwide <jxwide@gmail.com>
2023-05-14 22:56:11 +0300

    workspaces, users, groups service: added filter for secure private cross-services methods

---
+++ users-service/src/main/java/ru/fllcker/usersservice/filters/PrivateFilter.java
@@ -0,0 +1,30 @@
+package ru.fllcker.usersservice.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@WebFilter(urlPatterns = "/users/private/**")
+public class PrivateFilter implements Filter {
+    @Value("${private.secret}")
+    private String secret;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        String privateSecret = ((HttpServletRequest) request).getHeader("PrivateSecret");
+
+        if (privateSecret != null && privateSecret.equals(secret)) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}
