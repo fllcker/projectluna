4add6d83cb65fb3289b3ce669e09451060cc079b
jxwide <jxwide@gmail.com>
2023-05-14 23:07:52 +0300

    added HttpUtils class with generateWithPrivateHeader method to these services: auth, groups, invitations, tasks, workspaces

---
+++ invitations-service/src/main/java/ru/fllcker/invitationsservice/utils/HttpUtils.java
@@ -0,0 +1,17 @@
+package ru.fllcker.invitationsservice.utils;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
+public class HttpUtils {
+    @Value("${private.secret}")
+    private static String secret;
+
+    public static HttpEntity<String> generateWithPrivateHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("PrivateSecret", secret);
+        return new HttpEntity<>(headers);
+    }
+}
